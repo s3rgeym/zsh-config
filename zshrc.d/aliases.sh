7b96aae5c81afe8e0eb8f4f44c601949a5d3034f
Sergey M <12753171+s3rgeym@users.noreply.github.com>
2025-06-03 23:19:03 +0300

    Update aliases.sh

--- zshrc.d/aliases.sh
+++ zshrc.d/aliases.sh
@@ -1,5 +1,6 @@
-alias zshrc="vim $ZDOTDIR/.zshrc"
-alias vi=nvim
 alias vim=nvim
+alias vi=vim
 alias vimdiff='nvim -d'
 alias nvimrc="nvim ~/.config/nvim/init.lua"
+alias vimrc=nvimrc
+alias zshrc="vim $ZDOTDIR/.zshrc"
